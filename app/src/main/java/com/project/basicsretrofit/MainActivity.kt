8731036429ef6e8a2be86acf3c2830448269a918
Sunsyy08 <seonhyben864@gmail.com>
2025-10-15 23:39:35 +0900

    ADD Basics

--- app/src/main/java/com/project/basicsretrofit/MainActivity.kt
+++ app/src/main/java/com/project/basicsretrofit/MainActivity.kt
@@ -18,30 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BasicsRetrofitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            BasicsRetrofitTheme { }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BasicsRetrofitTheme {
-        Greeting("Android")
     }
 }
